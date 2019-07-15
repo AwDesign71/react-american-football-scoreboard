842482fb51d4c1732bcebdd3abdad62f7acb2039
React Learning <amwilson2019@gmail.com>
2019-07-15 15:59:46 -0400

    Add state import, deconstructor, and onClick

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, {useState} from 'react';
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+const [tigers, home] = useState(32);
 
   return (
     <div className="container">
@@ -15,7 +16,7 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{tigers}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
@@ -28,7 +29,7 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={()=>home(tigers + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
