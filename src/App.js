afc066b180d2117b5c6ef40771d0259774085d60
React Learning <amwilson2019@gmail.com>
2019-07-15 17:48:22 -0400

    working styling for the stretch problems

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const [tigers, vistor] = useState(32);
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            {/* <h2 className="home__name">Lions</h2> */}
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -20,7 +20,7 @@ const [tigers, vistor] = useState(32);
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+           {/*  <h2 className="away__name">Tigers</h2> */}
             <div className="away__score">{tigers}</div>
           </div>
         </div>
